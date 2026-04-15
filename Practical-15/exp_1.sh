344867e97aa18aa502aa21a02795a611b62f8679
Piyush Agrawal <piyusha871@gmail.com>
2026-04-15 09:42:16 +0530

    Create exp_1.sh

---
+++ Practical-15/exp_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Enter the option"
+echo "a = print the date"
+echo "b = list all files in directory"
+read choice
+
+case $choice in
+a) date ;;
+b) ls ;;
+*) echo "Not a valid option" ;;
+esac
